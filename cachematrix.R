0bd9ea7d19e1f28be3e1d1413c4f6b86c19a085b
makoto <cmsandiga@gmail.com>
2015-09-27 01:01:04 -0300

    Functions for cache inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,32 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (x = matrix()) {
+  m <- NULL
+  set <- function (y){
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setIM <- function ( inverse ) m <<- inverse
+  getIM <- function() m
+  list( set = set, get = get, setIM = setIM, getIM = getIM)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getIM()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(m,  ...)
+  x$setIM(m)
+  m
 }
+
+m <- makeCacheMatrix(matrix(rnorm(9),3,3))
+cacheSolve(m)
